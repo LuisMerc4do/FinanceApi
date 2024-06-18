ce4ac4803377dc2b5ff1e0b54680ed8363f31528
LuisMerc4do <163725779+LuisMerc4do@users.noreply.github.com>
2024-06-18 18:19:00 +1000

    Migrations and database Update

---
+++ FinanceApi/Migrations/20240618081354_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FinanceApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "5b50994c-9b59-47e3-9652-2ddc29d43a3b", null, "User", "USER" },
+                    { "dcb5b1ec-f9f3-4549-bc55-f5a2be2c06f1", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5b50994c-9b59-47e3-9652-2ddc29d43a3b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dcb5b1ec-f9f3-4549-bc55-f5a2be2c06f1");
+        }
+    }
+}

7097173a2d8ce96809ab06c2e225c24e9e30388a
LuisMerc4do <163725779+LuisMerc4do@users.noreply.github.com>
2024-06-22 19:19:36 +1000

    Design Page

---
+++ Frontend/frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const DesingPage = (props: Props) => {
+  return (
+    <>
+      <h1>FinanceAPP Design Page</h1>
+      <h2>This is where we well test various designs aspects of the app</h2>
+      <Table />
+    </>
+  );
+};
+
+export default DesingPage;

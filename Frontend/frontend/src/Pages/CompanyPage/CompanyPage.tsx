b1db171fe893343cfbc8ebe3c117ed38b63491ce
LuisMerc4do <163725779+LuisMerc4do@users.noreply.github.com>
2024-06-23 13:40:40 +1000

    Number Formatting, Final changes

--- Frontend/frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ Frontend/frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -5,6 +5,7 @@ import { getCompanyProfile } from "../../api";
 import Sidebar from "../../Components/Sidebar/Sidebar";
 import CompanyDashboard from "../../Components/CompanyDashboard/CompanyDashboard";
 import Tile from "../../Components/Tile/Tile";
+import Spinner from "../../Components/Spinner/Spinner";
 
 interface Props {}
 
@@ -25,17 +26,19 @@ const CompanyPage = (props: Props) => {
         <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
           <Sidebar />
           <CompanyDashboard ticker={ticker!}>
-            <Tile title="Company Name" subTitle={company.companyName}></Tile>
-            <Tile title="Price" subTitle={company.price.toString()}></Tile>
-            <Tile title="Sector" subTitle={company.sector}></Tile>
-            <Tile title="Dfc" subTitle={company.dcf.toString()}></Tile>
-            <p className="bg-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4">
+            <Tile title="Company Name" subTitle={company.companyName} />
+            <Tile title="Price" subTitle={"$" + company.price.toString()} />
+            <Tile title="DCF" subTitle={"$" + company.dcf.toString()} />
+            <Tile title="Sector" subTitle={company.sector} />
+            {/*<CompFinder ticker={company.symbol} />
+            <TenKFinder ticker={company.symbol} />*/}
+            <p className="bg-white shadow rounded text-medium font-medium text-gray-900 p-3 mt-1 m-4">
               {company.description}
             </p>
           </CompanyDashboard>
         </div>
       ) : (
-        <div>Company not Found!</div>
+        <Spinner />
       )}
     </>
   );
